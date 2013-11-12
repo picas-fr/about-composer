e19e10367a4d2826cfd5b431ae1187c221ab32db
picas-fr <picas@smile.fr>
2013-11-12 16:55:03 +0100

    PHP class for root package autoloader test

---
+++ src/TestNamespace/TestClass.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @see http://github.com/picas-fr/about-composer
+ */
+
+namespace TestNamespace;
+
+
+class TestClass
+{
+    
+    public static function myTest()
+    {
+        return "OK - Test works, class '".__CLASS__."' is loaded.";
+    }
+    
+}
